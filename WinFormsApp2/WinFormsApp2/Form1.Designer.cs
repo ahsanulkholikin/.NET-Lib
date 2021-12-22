3480690cc9f8124a5297fbccba46c0044efd9e09
ahsannul kholikin <ahsanulkholikin99@gmail.com>
2021-12-22 17:02:16 +0700

    Customer Crud Sql Server

---
+++ WinFormsApp2/WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txtcaridata = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtnama = new System.Windows.Forms.TextBox();
+            this.txtvoucer = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dgvcustomer = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcustomer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.txtcaridata,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(395, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(95, 22);
+            this.toolStripLabel1.Text = "Pencarian Data : ";
+            // 
+            // txtcaridata
+            // 
+            this.txtcaridata.Name = "txtcaridata";
+            this.txtcaridata.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID Cus.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama Cus.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Voucher";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(145, 58);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(223, 23);
+            this.txtid.TabIndex = 4;
+            // 
+            // txtnama
+            // 
+            this.txtnama.Location = new System.Drawing.Point(145, 88);
+            this.txtnama.Name = "txtnama";
+            this.txtnama.Size = new System.Drawing.Size(223, 23);
+            this.txtnama.TabIndex = 5;
+            // 
+            // txtvoucer
+            // 
+            this.txtvoucer.Location = new System.Drawing.Point(145, 119);
+            this.txtvoucer.Name = "txtvoucer";
+            this.txtvoucer.Size = new System.Drawing.Size(223, 23);
+            this.txtvoucer.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(115, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(212, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(293, 173);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dgvcustomer
+            // 
+            this.dgvcustomer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvcustomer.Location = new System.Drawing.Point(25, 222);
+            this.dgvcustomer.Name = "dgvcustomer";
+            this.dgvcustomer.RowTemplate.Height = 25;
+            this.dgvcustomer.Size = new System.Drawing.Size(343, 150);
+            this.dgvcustomer.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 431);
+            this.Controls.Add(this.dgvcustomer);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtvoucer);
+            this.Controls.Add(this.txtnama);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcustomer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox txtcaridata;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtnama;
+        private System.Windows.Forms.TextBox txtvoucer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dgvcustomer;
+    }
+}
+
---
+++ WinFormsApp2/WinFormsApp2/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-KHG6T1S;Initial Catalog=kantinAmikom;Integrated Security=True");
+
+        private void resetdata()
+        {
+            txtid.Text = "";
+            txtnama.Text = "";
+            txtvoucer.Text = "";
+
+        }
+        private void showdata()
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from customer";
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "customer");
+            dgvcustomer.DataSource = ds;
+            dgvcustomer.DataMember = "customer";
+
+            dgvcustomer.ReadOnly = true;
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetdata();
+            showdata();
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from customer where namaCustomer like '%"+txtcaridata.Text+"%'";
+
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "customer");
+            dgvcustomer.DataSource = ds;
+            dgvcustomer.DataMember = "customer";
+
+            dgvcustomer.ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtid.Text == "" | txtnama.Text == ""| txtvoucer.Text=="")
+            {
+                MessageBox.Show("Semua data harus terisi!", "Peringatan");
+                goto berhenti;
+            }
+            int num;
+            // membuat variabel isNum kemudian isi dengan num itu sendiri
+            bool isNum = int.TryParse(txtvoucer.Text.ToString(), out num);
+
+            if (!isNum)
+            {
+                MessageBox.Show("Isi voucher harus angka", "Peringatan");
+                goto berhenti;
+            }
+
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Insert into customer values ('" +txtid.Text+ "','"+txtnama.Text
+                +"','"+int.Parse(txtvoucer.Text)+"')";
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            showdata();
+            resetdata();
+
+
+
+
+        berhenti:
+            ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtid.Text == "" | txtnama.Text == "" | txtvoucer.Text == "")
+            {
+                MessageBox.Show("Semua data harus terisi!", "Peringatan");
+                goto berhenti;
+            }
+            int num;
+            // membuat variabel isNum kemudian isi dengan num itu sendiri
+            bool isNum = int.TryParse(txtvoucer.Text.ToString(), out num);
+
+            if (!isNum)
+            {
+                MessageBox.Show("Isi voucher harus angka", "Peringatan");
+                goto berhenti;
+            }
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Update customer set namaCustomer ='" + txtnama.Text + "', nominalIsi ='" + int.Parse(txtvoucer.Text)
+                + "' where idCustomer ='" + txtid.Text + "'";
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            showdata();
+            resetdata();
+
+
+
+
+        berhenti:
+            ;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (txtid.Text == "")
+            {
+                MessageBox.Show("Isi id cutomer yang akan dihapus!", "Peringatan");
+                goto berhenti;
+            }
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Delete from customer where idCustomer ='"+txtid.Text+"'";
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            showdata();
+            resetdata();
+
+        berhenti:
+            ;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            showdata();
+        }
+    }
+}
