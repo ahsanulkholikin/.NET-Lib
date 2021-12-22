8e1961b7a0e541505aaa7205265505809d76f27e
ahsannul kholikin <ahsanulkholikin99@gmail.com>
2021-12-22 16:52:36 +0700

    praktikum sekian

---
+++ Prak8/Prak8/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Prak8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        sqlConnection con = new sqlConnection(@"Data Source=DESKTOP-KHG6T1S;Initial Catalog=kantinAmikom;Integrated Security=True");
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Prak8/Prak8/Koneksi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//using System.Data.SqlClient;
+using System.Data.SqlClient;
+namespace Prak8
+{
+    class Koneksi
+    {
+        public SqlConnection GetConn()
+        {
+            sqlConnection Conn = new sqlConnection();
+            Conn.ConnectionString = 
+        }
+    }
+}
